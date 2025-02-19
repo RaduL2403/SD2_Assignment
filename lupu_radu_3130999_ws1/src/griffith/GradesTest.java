31d228eacdd8b03915842d285a8fc23793bdd2fc
Radu Lupu <raduconstantin.lupu@student.griffith.ie>
2025-02-19 11:00:31 +0000

    Created java file Grades

---
+++ lupu_radu_3130999_ws1/src/griffith/GradesTest.java
@@ -0,0 +1,17 @@
+package griffith;
+import org.junit.Test;
+import org.junit.Before;    
+    
+public class GradesTest {
+
+    @Before
+    public void setup(){
+
+    }
+        
+    @Test
+    public void test() {
+        
+    }
+}
+    
