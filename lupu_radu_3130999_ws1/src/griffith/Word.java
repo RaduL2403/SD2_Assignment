8c45f06fbd710d1f8d93459dfa167ea69084cfa1
Radu Lupu <raduconstantin.lupu@student.griffith.ie>
2025-02-19 20:00:20 +0000

    Successful tests Task2

--- lupu_radu_3130999_ws1/src/griffith/Word.java
+++ lupu_radu_3130999_ws1/src/griffith/Word.java
@@ -7,22 +7,28 @@ public class Word {
 
     //constructor
     public Word(char[] letters) {
-        // if (letters == null) {
-        //     throw new IllegalArgumentException("Word cannot be null");
-        // }
-        // this.letters = letters;
+        if (letters == null) {
+            throw new IllegalArgumentException("Word cannot be null");
+        }
+        this.letters = letters;
     }
 
 
     //Stub methods
     public boolean contains(char symbol){
+        for (char letter : letters) {
+            if (letter == symbol) {
+                return true;
+            }
+            
+        }
         return false;
     }
     public int length(){
-        return 0;
+        return this.letters.length;
     }
     public char[] getLetters(){
-        return null;
+        return this.letters;
     }
 
 }
--- lupu_radu_3130999_ws1/src/griffith/WordTest.java
+++ lupu_radu_3130999_ws1/src/griffith/WordTest.java
@@ -1,22 +1,26 @@
 package griffith;
-import org.junit.Test; 
-    
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class WordTest {
-        
+
     @Test
     public void testContains() {
-        
+        Word word = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+        assertTrue(word.contains('e'));
+        assertFalse(word.contains('z'));
     }
 
     @Test
     public void testLength() {
-        
+        Word word = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+        assertEquals(5, word.length());
     }
 
     @Test
     public void testNotNull() {
-        
+        Word word = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+        assertNotNull(word.getLetters());
+   
     }
-    
 }
-    
