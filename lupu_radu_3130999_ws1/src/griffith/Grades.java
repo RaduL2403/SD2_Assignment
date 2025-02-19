13af0f269c935ddec574869c689167b40d56d0f6
Radu Lupu <raduconstantin.lupu@student.griffith.ie>
2025-02-19 12:21:35 +0000

    Updated code - Successful tests

--- lupu_radu_3130999_ws1/src/griffith/Grades.java
+++ lupu_radu_3130999_ws1/src/griffith/Grades.java
@@ -2,17 +2,44 @@ package griffith;
 
 public class Grades {
     
-    public static int gradesMax(int[] grades){
-        return 0;
+    public static int gradesMax(int[] grades) {
+        if (grades.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
+        int max = grades[0];
+        for (int grade : grades) {
+            if (grade > max) {
+                max = grade;
+            }
+        }
+        return max;
     }
-    public static int gradesTotal ( int[] grades){
-        return 0;
+
+    public static int gradesTotal(int[] grades) {
+        int total = 0;
+        for (int grade : grades) {
+            total += grade;
+        }
+        return total;
     }
-    public static  double gradesAverage( int[] grades){
-        return 0;
+
+    public static double gradesAverage(int[] grades) {
+        if (grades.length == 0) {
+            throw new IllegalArgumentException("Array cannot be empty");
+        }
+        double total = gradesTotal(grades);
+        return total / grades.length;
     }
-    public static int countFails(int[] grades, int minGrade){
-        return 0;
+
+
+    public static int countFails(int[] grades, int minGrade) {
+        int count = 0;
+        for (int grade : grades) {
+            if (grade < minGrade) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
--- lupu_radu_3130999_ws1/src/griffith/GradesTest.java
+++ lupu_radu_3130999_ws1/src/griffith/GradesTest.java
@@ -8,9 +8,46 @@ public class GradesTest {
 
     
     @Test
-    public void test(){
+    public void testGradesMax() {
+        int[] grades1 = {70, 85, 90, 100};
+        int[] grades2 = {5, 0, 5, 11};
+        int[] grades3 = {};
         
+        assertEquals(100, Grades.gradesMax(grades1));
+        assertEquals(11, Grades.gradesMax(grades2));
+        assertThrows(IllegalArgumentException.class, () -> Grades.gradesMax(grades3));
+    }
+
+    @Test
+    public void testGradesTotal() {
+        int[] grades1 = {10, 20, 30};
+        int[] grades2 = {7, 3, 0, 5};
+        int[] grades3 = {};
+        
+        assertEquals(60, Grades.gradesTotal(grades1));
+        assertEquals(15, Grades.gradesTotal(grades2));
+        assertEquals(0, Grades.gradesTotal(grades3));
+    }
+
+    @Test
+    public void testGradesAverage() {
+        int[] grades1 = {10, 20, 30, 40};
+        int[] grades2 = {10, 26, 30};
+        int[] grades3 = {};
+        
+        assertEquals(25.0, Grades.gradesAverage(grades1), 0.001);
+        assertEquals(22.0, Grades.gradesAverage(grades2), 0.001);
+        assertThrows(IllegalArgumentException.class, () -> Grades.gradesAverage(grades3));
+    }
+
+    @Test
+    public void testCountFails() {
+        int[] grades1 = {10, 50, 60, 20};
+        int[] grades2 = {5, 15, 25, 35};
+        int[] grades3 = {};
+        
+        assertEquals(2, Grades.countFails(grades1, 40));
+        assertEquals(3, Grades.countFails(grades2, 30));
+        assertEquals(0, Grades.countFails(grades3, 50));
     }
 }
-
-    
